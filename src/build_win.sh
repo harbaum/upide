ea7a64ad9b1e10a82e252b3d91e1834fa463365c
Till Harbaum <till@harbaum.org>
2022-04-17 15:00:48 +0200

    Unify build scripts

--- src/build_win.sh
+++ src/build_win.sh
@@ -4,4 +4,4 @@
 # wine python-3.9.5-amd64.exe
 # wine "$PY"/Scripts/pip install pyinstaller pyqt5 pyserial
 PY=~/.wine/drive_c/users/$USER/Local\ Settings/Application\ Data/Programs/Python/Python39
-wine "${PY}/Scripts/pyinstaller" --clean --icon=upide.ico --noconsole --onefile --add-data='assets;assets' upide.py
+wine "${PY}/python" -m PyInstaller --clean --icon=upide.ico --windowed --onefile --add-data='assets;assets' upide.py
