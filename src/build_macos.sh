fe14ac7c480b83d8d3e80f19cf174d7c47719497
Andy Piper <andypiper@users.noreply.github.com>
2022-05-05 13:26:26 +0100

    src/build_macos.sh: Fixes for universal macOS builds.

--- src/build_macos.sh
+++ src/build_macos.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-python3 -m PyInstaller --noconfirm --windowed --name="upide" --icon=upide.icns --add-data='assets:assets' upide.py
+# requires create-dmg (brew install create-dmg)
+python3 -m PyInstaller --target-arch=universal2 --noconfirm --windowed --name="upide" --icon=upide.icns --add-data='assets:assets' upide.py
 mkdir -p dist/dmg
 rm -r dist/dmg/*
 cp -r dist/upide.app dist/dmg
