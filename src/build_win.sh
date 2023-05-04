9bc0819c1481730f5307448ab627bd55c1ea0d6c
harbaum <till.harbaum@1und1.de>
2023-05-04 13:55:34 +0200

    Build win version with Python 3.10

--- src/build_win.sh
+++ src/build_win.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Needed in preparation:
-# wget https://www.python.org/ftp/python/3.9.5/python-3.9.5-amd64.exe
-# wine python-3.9.5-amd64.exe
-# wine "$PY"/Scripts/pip install pyinstaller pyqt5 pyserial
-PY=~/.wine/drive_c/users/$USER/Local\ Settings/Application\ Data/Programs/Python/Python39
-wine "${PY}/python" -m PyInstaller --clean --icon=upide.ico --windowed --onefile --add-data='assets;assets' upide.py
+# wget https://www.python.org/ftp/python/3.10.11/python-3.10.11-amd64.exe
+# wine python-3.10.11-amd64.exe
+# wine "$PY"/Scripts/pip.exe install pyinstaller pyqt5 pyserial
+PY=~/.wine/drive_c/users/$USER/AppData/Local/Programs/Python/Python310
+wine "${PY}/python.exe" -m PyInstaller --clean --icon=upide.ico --windowed --onefile --add-data='assets;assets' upide.py
