111b814b7136ac5ccc7b0c844303eddb58158887
Till Harbaum <till@harbaum.org>
2023-05-05 16:35:04 +0200

    Fix win build script

--- src/build_win.sh
+++ src/build_win.sh
@@ -4,4 +4,9 @@
 # wine python-3.10.11-amd64.exe
 # wine "$PY"/Scripts/pip.exe install pyinstaller pyqt5 pyserial
 PY=~/.wine/drive_c/users/$USER/AppData/Local/Programs/Python/Python310
+if [ ! -f "$PY/python.exe" ]; then
+    echo "$PY/python.exe does not exist."
+    PY=~/.wine/drive_c/users/$USER/Local\ Settings/Application\ Data/Programs/Python/Python310
+fi
+
 wine "${PY}/python.exe" -m PyInstaller --clean --icon=upide.ico --windowed --onefile --add-data='assets;assets' upide.py
